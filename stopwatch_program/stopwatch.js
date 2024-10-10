dd8f9f1c4f673e3cefcaafd751f44cb33884cae5
let <leticiacostaoa@gmail.com>
2024-10-10 18:48:59 -0300

    stopwatch_program

---
+++ stopwatch_program/stopwatch.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const display = document.getElementById("display");
+let timer = null, startTime = 0, elapsedTime = 0, isRunning = false;
+
+function start(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 20);
+        isRunning = true;
+    }
+}
+
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+
+function reset(){
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+}
+
+function update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    //converting the elapsed time to th einterface version
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let mins = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let secs = Math.floor(elapsedTime / 1000 % 60);
+    let miliseconds = Math.floor((elapsedTime % 1000) / 10); //getting just 2 digits
+
+    hours = String(hours).padStart(2, "0");
+    mins = String(mins).padStart(2, "0");
+    secs = String(secs).padStart(2, "0");
+    miliseconds = String(miliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${mins}:${secs}:${miliseconds}`;
+}
