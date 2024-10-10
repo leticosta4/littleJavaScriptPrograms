eebca1aadf9ec0802e2457efa5bf588d15e9e0a4
let <leticiacostaoa@gmail.com>
2024-10-10 16:33:22 -0300

    relogio ditigay - military time

--- digital_clock_program/dig_clock.js
+++ digital_clock_program/dig_clock.js
@@ -1,2 +1,13 @@
 'use strict'
 
+function updateClock(){
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, 0);
+    const mins = now.getMinutes().toString().padStart(2, 0);
+    const secs = now.getSeconds().toString().padStart(2, 0); 
+    const timeString = `${hours}:${mins}:${secs}`;
+
+    document.getElementById("clock").textContent = timeString; 
+}
+
+setInterval(updateClock, 1000);
