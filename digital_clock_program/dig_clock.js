367b8231a4d11115346e1bc6f048a39e752b8073
let <leticiacostaoa@gmail.com>
2024-10-10 16:42:56 -0300

    digital clock AM/PM version

--- digital_clock_program/dig_clock.js
+++ digital_clock_program/dig_clock.js
@@ -2,12 +2,18 @@
 
 function updateClock(){
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, 0);
+    let hours = now.getHours();
     const mins = now.getMinutes().toString().padStart(2, 0);
     const secs = now.getSeconds().toString().padStart(2, 0); 
-    const timeString = `${hours}:${mins}:${secs}`;
+
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0); 
+
+    const timeString = `${hours}:${mins}:${secs} ${meridiem}`;
 
     document.getElementById("clock").textContent = timeString; 
 }
 
+updateClock();
 setInterval(updateClock, 1000);
