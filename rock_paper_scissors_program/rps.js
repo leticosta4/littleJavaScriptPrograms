02339b96b498a53442f9c1ac41bb54bdd18e423d
let <leticiacostaoa@gmail.com>
2024-10-13 11:58:34 -0300

    incrementando o rock-paper-scissors mas ele ainda não tem responsividade

--- rock_paper_scissors_program/rps.js
+++ rock_paper_scissors_program/rps.js
@@ -4,6 +4,9 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0, computerScore = 0;
 
 function playGame(playerChoice){
     const computerChoice = choices[Math.floor(Math.random() * 3)];
@@ -27,4 +30,21 @@ function playGame(playerChoice){
     playerDisplay.textContent = `PLAYER: ${playerChoice}`;
     computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
     resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("greenText", "redText")
+
+    switch(result){
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            playerScore++;
+            console.log(`player: ${playerScore}`);
+            playerScoreDisplay.innerText = playerScore;
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            computerScore++;
+            console.log(`computer: ${computerScore}`);
+            computerScoreDisplay.innerText = computerScore;
+            break;
+    }    
 }
